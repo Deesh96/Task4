adf67fc206b9b36f7cb7e1cb85d9096bd209ca90
Deesh96 <73774912+Deesh96@users.noreply.github.com>
2021-02-09 14:45:53 +0530

    Task4_V8 Reflection07
    Adding Reflection class07 to change accessibility

--- Task4/src/KUnitR/Ref07.java
+++ Task4/src/KUnitR/Ref07.java
@@ -1,18 +1,17 @@
 package KUnitR;
 import java.lang.reflect.Field;
 
-//Checking accessibility
-public class Ref06 {
-
+public class Ref07 {
 	public static void main(String args[]) throws Exception{
 		SimpleM sm = new SimpleM();
-		
 		Field[] fields = sm.getClass().getDeclaredFields();
 		System.out.printf("There are %d fields\n", fields.length);
 		
+		//Changing the Accessibility
+		
 		for(Field f : fields) {
-			System.out.printf("Field name=%s type=%s accessible=%s\n", f.getName(), f.getType(), f.isAccessible());
+			f.setAccessible(true);
+			System.out.printf("Field name=%s type=%s value=%f\n",f.getName(), f.getType(), f.getFloat(sm));
 		}
 	}
-
 }
