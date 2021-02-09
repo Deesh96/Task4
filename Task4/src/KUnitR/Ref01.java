e902bb2c89b85783dc1b11c3d1a4f980c9146328
Deesh96 <73774912+Deesh96@users.noreply.github.com>
2021-02-09 13:40:11 +0530

    Task4_V4 Reflection03
    Adding Reflection class 03 to print Main class details

--- Task4/src/KUnitR/Ref01.java
+++ Task4/src/KUnitR/Ref01.java
@@ -2,8 +2,8 @@ package KUnitR;
 
 public class Ref01 {
 	public static void main(String args[]) {
-		SimpleM s = new SimpleM();
+		SimpleM sm = new SimpleM();
 		// Retrieve the values from the SimpleM class using an object
-		System.out.println("SM : " + s);
+		System.out.println("SM : " + sm);
 	}
 }
--- Task4/src/KUnitR/Ref02.java
+++ Task4/src/KUnitR/Ref02.java
@@ -5,12 +5,12 @@ public class Ref02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		SimpleM s = new SimpleM();
-		s.squareC();
+		SimpleM sm = new SimpleM();
+		sm.squareC();
 		
 		// Getting Values from private members of the simpleM class
 		//s.squareD();
-		System.out.println("SM : " + s);
+		System.out.println("SM : " + sm);
 	}
 
 }
