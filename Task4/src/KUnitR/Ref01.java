21e2d052045da4f50bfefbfd6a216447dfea5bbd
Deesh96 <73774912+Deesh96@users.noreply.github.com>
2021-02-09 13:34:17 +0530

    Task4_V3 Reflection02
    Adding Reflection class 02

--- Task4/src/KUnitR/Ref01.java
+++ Task4/src/KUnitR/Ref01.java
@@ -3,7 +3,7 @@ package KUnitR;
 public class Ref01 {
 	public static void main(String args[]) {
 		SimpleM s = new SimpleM();
-		// creating a class to retrieve the values from the Simple class using an object
-		System.out.println("S : " + s);
+		// Retrieve the values from the SimpleM class using an object
+		System.out.println("SM : " + s);
 	}
 }
---
+++ Task4/src/KUnitR/Ref02.java
@@ -0,0 +1,16 @@
+package KUnitR;
+
+public class Ref02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SimpleM s = new SimpleM();
+		s.squareC();
+		
+		// Getting Values from private members of the simpleM class
+		//s.squareD();
+		System.out.println("SM : " + s);
+	}
+
+}
