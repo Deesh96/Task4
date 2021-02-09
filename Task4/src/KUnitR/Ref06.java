83c826c89a5d059dd5ce85b2d8c59f89737f5b0f
Deesh96 <73774912+Deesh96@users.noreply.github.com>
2021-02-09 14:34:07 +0530

    Task4_V7 Reflection06
    Creating Reflection class 06 to  Check accessibility

---
+++ Task4/src/KUnitR/Ref06.java
@@ -0,0 +1,18 @@
+package KUnitR;
+import java.lang.reflect.Field;
+
+
+public class Ref06 {
+
+	public static void main(String args[]) throws Exception{
+		SimpleM sm = new SimpleM();
+		
+		Field[] fields = sm.getClass().getDeclaredFields();
+		System.out.printf("There are %d fields\n", fields.length);
+		
+		for(Field f : fields) {
+			System.out.printf("Field name=%s type=%s accessible=%s\n", f.getName(), f.getType(), f.isAccessible());
+		}
+	}
+
+}
